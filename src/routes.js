ea8148419c7e4d1a20cd050f9643a0d3f1b7318e
ambegossi <andersonbegossi@gmail.com>
2020-02-10 21:40:07 -0300

    Add Deliveryman management

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymanController from './app/controllers/DeliverymanController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -14,7 +19,15 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware); // Only applied for routes that come after
 
 routes.put('/users', UserController.update);
+
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:recipientId', RecipientController.update);
+
+routes.post('/deliverymens', DeliverymanController.store);
+routes.get('/deliverymens', DeliverymanController.index);
+routes.put('/deliverymens/:id', DeliverymanController.update);
+routes.delete('/deliverymens/:id', DeliverymanController.delete);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
