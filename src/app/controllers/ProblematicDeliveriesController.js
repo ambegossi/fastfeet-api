44643fd994dbd96d7cd671a17bd2d16a1d11590b
Anderson Begossi <andersonbegossi@gmail.com>
2020-04-16 10:08:49 -0300

    Added pagination on ProblematicDeliveriesController

--- src/app/controllers/ProblematicDeliveriesController.js
+++ src/app/controllers/ProblematicDeliveriesController.js
@@ -2,7 +2,13 @@ import DeliveryProblem from '../models/DeliveryProblem';
 
 class ProblematicDeliveriesController {
   async index(req, res) {
-    const deliveriesWithProblems = await DeliveryProblem.findAll();
+    const { page = 1 } = req.query;
+
+    const deliveriesWithProblems = await DeliveryProblem.findAll({
+      order: ['created_at'],
+      limit: 6,
+      offset: (page - 1) * 6,
+    });
     return res.json(deliveriesWithProblems);
   }
 }
